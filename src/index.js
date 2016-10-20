0f8f07b93824431c6ff820cdd29c124c3cbd02cf
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2016-10-21 00:08:25 +0200

    fix redraw

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React, {
-  Component,
+  PureComponent,
   PropTypes,
 } from "react";
 import JSON2D from "json2d";
 
-export default class Json2d extends Component {
+export default class Json2d extends PureComponent {
   static propTypes = {
     children: PropTypes.object.isRequired,
     width: PropTypes.number.isRequired,
@@ -16,31 +16,24 @@ export default class Json2d extends Component {
   static defaultProps = {
     ratio: window.devicePixelRatio,
   };
-  shouldComponentUpdate ({ width, height, ratio }) {
-    const { props } = this;
-    return props.width !== width ||
-      props.height !== height ||
-      props.ratio !== ratio;
-  }
   componentDidMount () {
     this.ctx = this._ref.getContext("2d");
     this.json2d = JSON2D(this.ctx, this.props.resolveImage);
     this.draw(this.props);
   }
-  componentDidUpdate (props) {
-    this.draw(props);
+  componentDidUpdate () {
+    this.draw(this.props);
   }
   draw ({ children, renderVisitor }) {
-    if (this._currentDrawn === children) return;
-    this._currentDrawn = children;
-    this.json2d.render(children, renderVisitor);
+    return this.json2d.render(children, renderVisitor);
   }
+  onRef = ref => { this._ref = ref; };
   render () {
     const { width, height, ratio } = this.props;
     return (
       <canvas
-        ref={ref => { this._ref = ref; }}
-        style={{ width: width+"px", height: height+"px" }}
+        ref={this.onRef}
+        style={{ width, height }}
         width={width * ratio}
         height={height * ratio}
       />
